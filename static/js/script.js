6e43430ad7dccf95d618dca7e7800211dc20df78
Ry2uko <gabrielfradz@hotmail.com>
2023-10-14 18:10:55 +0800

    Hamburger

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
   console.log('Document ready');
+
+  $('#menu-btn').on('click', function(){
+    $(this).toggleClass('open');
+    $('#menu').toggleClass('flex hidden');
+
+  });
 });
